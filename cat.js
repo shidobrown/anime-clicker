c345c3c0623918364556856002ab94981ad8a68c
jaylen phipps <jaylenphipps1@gmail.com>
2019-04-19 16:07:08 -0400

    work in progress

---
+++ cat.js
@@ -0,0 +1,72 @@
+
+var iy = 0;
+   function buttonClick() {
+       document.getElementById('inc').value = ++iy;
+   }
+   
+var fy = 0;
+function buttonClick1() {
+    document.getElementById('ppp').value = ++fy;
+}
+
+var ty = 0;
+   function buttonClick2() {
+       document.getElementById('ooo').value = ++ty;
+   }
+   
+var cy = 0;
+function buttonClick3() {
+    document.getElementById('iii').value = ++cy;
+}
+
+
+
+var a = 0;
+   function buttonClicka() {
+       document.getElementById('a').value = ++a;
+   }
+   
+var b = 0;
+function buttonClickb() {
+    document.getElementById('b').value = ++b;
+}
+
+var c = 0;
+   function buttonClickc() {
+       document.getElementById('c').value = ++c;
+   }
+   
+var d = 0;
+function buttonClickd() {
+    document.getElementById('d').value = ++d;
+}
+
+
+
+var e = 0;
+   function buttonClicke() {
+       document.getElementById('e').value = ++e;
+   }
+   
+var f = 0;
+function buttonClickf() {
+    document.getElementById('f').value = ++f;
+}
+
+var g = 0;
+   function buttonClickg() {
+       document.getElementById('g').value = ++g;
+   }
+   
+var h = 0;
+function buttonClickh() {
+    document.getElementById('h').value = ++h;
+}
+var i = 0;
+function buttonClicki() {
+    document.getElementById('i').value = ++i;
+}
+
+
+
+
